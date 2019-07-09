2ef4c8facfe23d3e539b877686bbe2027b5b29a3
Mike Tracz <miketracz@hotmail.com>
2019-07-09 18:42:16 +0000

    Video Uploading

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'flixter-freshcup.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
-https://flixter-freshcup.herokuapp.com/
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
