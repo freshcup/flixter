7c350db8d0f9e13b02bbd156cf669bddd6ea8d90
Mike Tracz <miketracz@hotmail.com>
2019-07-10 20:03:17 +0000

    student enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,13 @@
 class Course < ActiveRecord::Base
-  belongs_to :user
-  has_many :sections
-
   mount_uploader :file, FileUploader
 
+  belongs_to :user
+  has_many :sections
+  has_many :enrollments
+
+  
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
+ 
